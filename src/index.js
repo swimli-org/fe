79d68c9d156901de88ed6e1caa48777c8abc54ed
Zach Young <zachyoung@Zachs-MacBook-Pro.local>
2020-08-22 00:25:16 -0500

    rewording

--- src/index.js
+++ src/index.js
@@ -21,6 +21,7 @@ import Covers from './views/Covers'
 import Cart from './views/Cart'
 import Register from './views/Register'
 import SearchResults from './views/SearchResults'
+import Checkout from './views/Checkout'
 
 import Footer from './components/Footer'
 
@@ -65,6 +66,10 @@ ReactDOM.render(
       </Route>
       <Route exact path="/">
             <App />
+
+      </Route>
+      <Route exact path="/checkout">
+            <Checkout />
 
       </Route>
     </Switch>
