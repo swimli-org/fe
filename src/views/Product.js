dfe843d05a5500d27990148c8027018bf22024e9
Dylan Baker <38574400+dbvker@users.noreply.github.com>
2020-08-31 02:12:33 -0500

    Updated code a bit to be more dynamic

--- src/views/Product.js
+++ src/views/Product.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import '../styles/Product.css'
+import intelliflo from '../assets/products/intelliflo.jpg'
 
 export default function Product() {
     return (
       <div className="container">
         <div className='breadcrumb-trail'>Equipment / Pumps & Motors</div>
-        <div className='product-image'>Image</div>
+        <div className='product-image-container'>
+        <img src={intelliflo} className='product-image' alt='Intelliflo'></img>
+
+        </div>
         <div className='product-header'>
             <div className='product-header-left'>
                 <div className='product-brand'>By Pentair</div>
